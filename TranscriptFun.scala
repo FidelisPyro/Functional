fcfd139d1e88dea6b9753b1c6a017682e2637a92
Kyle Lofthus <98364709+FidelisPyro@users.noreply.github.com>
2023-09-22 07:53:32 -0600

    Update TranscriptFun.scala

--- TranscriptFun.scala
+++ TranscriptFun.scala
@@ -1,7 +1,7 @@
 
 //Kyle Lofthus
 
-
+//creates Grade that holds letter grades, and their equivalent gpa number
 enum Grade(val letter: String, val num_grade: Double):
     case A extends Grade("A", 4.0)
     case B extends Grade("B", 3.0)
@@ -10,12 +10,13 @@ enum Grade(val letter: String, val num_grade: Double):
     case F extends Grade("F", 0.0)
 end Grade
 
-
+//creates the case class Records to hold the course, grade, and credits
 case class Record(course_num: String, grade: Grade, credits: Int)
 
+//creates alias of a list of records called TranscriptData
 type TranscriptData = List[Record]
 
-
+//recursively goes through the TranscriptData to calculate the weighted gpa
 def gpaFun(records: TranscriptData): Double = 
     {
 		def calculateGPA(records: TranscriptData, gpaCredit: Double, credit_total: Int): Double =
@@ -29,6 +30,9 @@ def gpaFun(records: TranscriptData): Double =
 		calculateGPA(records, 0.0, 0)
 	}
 
+//recursively goes through the TranscriptData and if any grade is < B then returns false
+//this assumes there is no empty list, because checking if a student who has not taken
+//any classes is on the honor roll, doesn't make since.
 def isHonorRollFun(records: TranscriptData): Boolean =
 	{
 		records match
@@ -39,10 +43,28 @@ def isHonorRollFun(records: TranscriptData): Boolean =
 				else false
 	}
 
+//recursively goes through the TranscriptData and if any course number matches the
+// the new record to be added, then that course is already there and no need to add it.
+//If I have time before due date, I will add that if they match the new one replaces
+//the old one (student retook class for a "hopefully better grade")
 def addFun(transcript: TranscriptData, record:Record): TranscriptData =
 	{
 		transcript match
 			case head :: tail => if head.course_num == record.course_num then transcript
-				else addFun(tail, record)
-			case Nil => val new_transcript: TranscriptData = transcript ::: List(record)
+				else head :: addFun(tail, record)
+			case Nil => transcript ::: List(record)
 	}
+
+
+//recursively goes through the TranscriptData and if it finds the course we're looking
+//for it checks if the grade for that course is a C or better (passing) and returns
+//true if it is and false if it isn't. It also returns false if the course isn't found.
+//Can't pass a course you haven't taken.
+def containsFun(transcript: TranscriptData, course: String): Boolean = 
+    {
+        transcript match
+            case head :: tail => if head.course_num == course 
+                then if head.grade.num_grade >= 2.0 then true else false
+                else containsFun(tail, course)
+            case Nil => false
+    }
