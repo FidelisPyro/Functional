6aac1a93502002ee7ff9c4f41d8fb0af98bc6d53
Kyle Lofthus <98364709+FidelisPyro@users.noreply.github.com>
2023-09-22 07:53:44 -0600

    Update TranscriptFunSuite.scala

--- TranscriptFunSuite.scala
+++ TranscriptFunSuite.scala
@@ -1,4 +1,4 @@
-
+//Kyle Lofthus
 
 import munit.ScalaCheckSuite
 import org.scalacheck.Prop._
@@ -39,7 +39,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
     }
 
     test("isHonorRollFun test with a C"){
-        val t:TranscriptData = List(Record("cosc1030", Grade.C, 4), Record("cosc2030", Grade.B, 4))
+        val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4))
         val obtained = isHonorRollFun(t)
         val expected:Boolean = false
         assertEquals(obtained, expected)
@@ -47,23 +47,71 @@ class TranscriptFunSuite extends ScalaCheckSuite
 
     test("simple addFun test"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.B, 4))
-		val new_record = Record("cosc3020", Grade.C, 3)
+        val new_record = Record("cosc3020", Grade.C, 3)
         val obtained = addFun(t, new_record)
         val expected:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.B, 4), Record("cosc3020", Grade.C, 3))
         assertEquals(obtained, expected)
     }
 
+    test("simple containsFun passing test"){
+        val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4), Record("cosc3020", Grade.F, 3))
+        val obtained = containsFun(t, "cosc2030")
+        val expected:Boolean = true
+        assertEquals(obtained, expected)
+    }
 
-/*
-  property("cross product is almost commutative") {
-    forAll { (x: Vec3, y: Vec3) =>
-      val crosspro:Vec3 = cross(x,y)
-      val negy:Vec3 = (-y(0), -y(1), -y(2))
-      val crossproCom:Vec3 = cross(negy, x)
-      assertEquals(crosspro._1, crossproCom._1)
-      assertEquals(crosspro._2, crossproCom._2)
-      assertEquals(crosspro._3, crossproCom._3)
+      test("simple containsFun failing test"){
+        val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4), Record("cosc3020", Grade.F, 3))
+        val obtained = containsFun(t, "cosc3020")
+        val expected:Boolean = false
+        assertEquals(obtained, expected)
+    }
+
+      test("containsFun test where the course isn't there"){
+        val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4))
+        val obtained = containsFun(t, "cosc3020")
+        val expected:Boolean = false
+        assertEquals(obtained, expected)
+    }
+
+
+    property("Adding a record increases the length of a transcript") {
+    forAll { (t: TranscriptData, record: Record) =>
+      val original_len: Int = t.length
+      val t_added: TranscriptData = addFun(t, record)
+      val new_len: Int = t_added.length
+      assertEquals(new_len, (original_len + 1))
     }
   }
-  */
+
+    property("Adding a passing record will mean the added course is contained in the new transcript") {
+    forAll { (t: TranscriptData, record: Record) =>
+      val t_added: TranscriptData = addFun(t, record)
+      val obtained = containsFun(t, record.course_num)
+      if obtained == true
+        then assertEquals(obtained, true)
+        else assertEquals(obtained, false)
+    }
+  }
+
+    property("The GPA is always between 0 and 4") {
+    forAll { (t: TranscriptData) =>
+      val gpa: Double = gpaFun(t)
+      if gpa >= 0 & gpa <= 4
+        then true
+        else false
+    }
+  }
+
+    property("The GPA increases (decreases) when you add a record with a higher (lower) grade than the old GPA") {
+    forAll { (t: TranscriptData, record: Record) =>
+      val old_gpa: Double = gpaFun(t)
+      val t_added: TranscriptData = addFun(t, record)
+      val new_gpa: Double = gpaFun(t_added) 
+      if old_gpa >= record.grade.num_grade
+        then old_gpa >= new_gpa
+        else old_gpa <= new_gpa
+    }
+  }
+
 }
