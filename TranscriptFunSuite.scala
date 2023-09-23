8974965e53705de0c6b07022f47d9764ec909536
Kyle Lofthus <98364709+FidelisPyro@users.noreply.github.com>
2023-09-22 21:10:07 -0600

    Update TranscriptFunSuite.scala

--- TranscriptFunSuite.scala
+++ TranscriptFunSuite.scala
@@ -7,6 +7,9 @@ import org.scalacheck.Prop._
 import org.scalacheck.Gen
 import org.scalacheck.Arbitrary
 
+
+//I copied the you provided for generating random entries, but I added that the
+//course cannot be an empty string. There is no course that doesn't have a name.
 import Grade._ 
 val recordGen = for {
   course  <- Gen.alphaStr.suchThat(_.nonEmpty)
@@ -19,13 +22,14 @@ implicit lazy val recordArbitrary : Arbitrary[Record] = Arbitrary(recordGen)
 
 class TranscriptFunSuite extends ScalaCheckSuite
 {
+    //testing gpaFun
     test("simple gpaFun test"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.B, 4))
         val obtained = gpaFun(t)
         val expected:Double = 3.50
         assertEquals(obtained, expected)
     }
-
+    //testing gpaFun with 0 credits 
     test("gpaFun test with 0 credits"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 0), Record("cosc2030", Grade.B, 0))
         val obtained = gpaFun(t)
@@ -33,6 +37,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
         assertEquals(obtained, expected)
     }
 
+    //testing isHonorRollFun
     test("simple isHonorRollFun test"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.B, 4))
         val obtained = isHonorRollFun(t)
@@ -40,6 +45,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
         assertEquals(obtained, expected)
     }
 
+    //testing isHonorRollFun with a C to provoke a false case
     test("isHonorRollFun test with a C"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4))
         val obtained = isHonorRollFun(t)
@@ -47,6 +53,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
         assertEquals(obtained, expected)
     }
 
+    //addFun test
     test("simple addFun test"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.B, 4))
         val new_record = Record("cosc3020", Grade.C, 3)
@@ -55,6 +62,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
         assertEquals(obtained, expected)
     }
 
+    //containsFun test where passing is true
     test("simple containsFun passing test"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4), Record("cosc3020", Grade.F, 3))
         val obtained = containsFun(t, "cosc2030")
@@ -62,6 +70,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
         assertEquals(obtained, expected)
     }
 
+      //containsFun test where passing is false
       test("simple containsFun failing test"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4), Record("cosc3020", Grade.F, 3))
         val obtained = containsFun(t, "cosc3020")
@@ -69,6 +78,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
         assertEquals(obtained, expected)
     }
 
+      //containsFun test where there course is not in the TranscriptData
       test("containsFun test where the course isn't there"){
         val t:TranscriptData = List(Record("cosc1030", Grade.A, 4), Record("cosc2030", Grade.C, 4))
         val obtained = containsFun(t, "cosc3020")
@@ -76,7 +86,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
         assertEquals(obtained, expected)
     }
 
-
+    //property to prove that adding a record increases the length of the transcript
     property("Adding a record increases the length of a transcript") {
     forAll { (t: TranscriptData, record: Record) =>
       val original_len: Int = t.length
@@ -86,6 +96,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
     }
   }
 
+    //property to prove that adding a passing will add the course in the new transcript
     property("Adding a passing record will mean the added course is contained in the new transcript") {
     forAll { (t: TranscriptData, record: Record) =>
       val t_added: TranscriptData = addFun(t, record)
@@ -95,6 +106,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
     }
   }
 
+    //property to prove that GPA is always between 0 and 4
     property("The GPA is always between 0 and 4") {
     forAll { (t: TranscriptData) =>
       val gpa: Double = gpaFun(t)
@@ -104,6 +116,7 @@ class TranscriptFunSuite extends ScalaCheckSuite
     }
   }
 
+    //property to prove that if add a course that is lower/higher than the GPA then the gpa will go down/up respectively
     property("The GPA increases (decreases) when you add a record with a higher (lower) grade than the old GPA") {
     forAll { (t: TranscriptData, record: Record) =>
       val old_gpa: Double = gpaFun(t)
