7f46c7d9f75762abe09e4b610ded0ac25e2facba
Kyle Lofthus <98364709+FidelisPyro@users.noreply.github.com>
2023-09-22 21:09:25 -0600

    Update Transcript.scala

--- Transcript.scala
+++ Transcript.scala
@@ -2,11 +2,13 @@
 
 package transcript
 
-
+//class Transcript to hold all of the information that is needed for Transcript
 final class Transcript(val records: TranscriptData) 
 	{
+        //calculates GPA
 		def gpa: Double =
 			{
+                //wrapper function to recursively call itself to calculate the GPA
 				def getGPA(records: TranscriptData, gpaCredit: Double, credit_total: Int): Double =
 					{
 						records match
@@ -18,8 +20,10 @@ final class Transcript(val records: TranscriptData)
 				getGPA(records, 0.0, 0)
 			}
 
+        //checks if the student is on the Honor Roll
 		def isHonorRoll: Boolean =
 			{
+                //wrapper function to recursively call itself to check the transcript for grades
 				def checkHonorRoll(records: TranscriptData): Boolean =
 					{
 						records match
@@ -32,12 +36,14 @@ final class Transcript(val records: TranscriptData)
 				checkHonorRoll(records)
 			}
 		
+        //adds a record to the transcript 
 		def +(r: Record): Transcript =
 			{
 				val new_transcript: Transcript = Transcript(records ::: List(r))
 				new_transcript
 			}
 
+        //checks if a record is in a transcript and if that record has a passing grade.
 		def contains(r: Record): Boolean =
 			{
 				def doesItContain(transcript: TranscriptData, course: String): Boolean =
