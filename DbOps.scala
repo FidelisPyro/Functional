2c77c5554fa72a05834487ebd1d14495d76dc92a
Kyle Lofthus <98364709+FidelisPyro@users.noreply.github.com>
2023-10-25 08:19:08 -0600

    Update DbOps.scala

--- DbOps.scala
+++ DbOps.scala
@@ -1,3 +1,4 @@
+
 //Kyle Lofthus
 
 package base	
@@ -6,7 +7,7 @@ case class Employee(ID: Int, Name: String, Age: Int, Salary: Double)
 case class Department(dept_ID: Int, Name: String, Location: String)
 case class EmployeeDepartment(e_ID: Int, d_ID: Int)
 
-type Table = List[Tuple]
+
 
 val emps = List(
     Tuple.fromProductTyped(Employee(1, "Barney", 30, 100000.0)),
@@ -34,21 +35,68 @@ val worksIn = List(
 )
 
 
-def project(table: Table, colPred: (Int) => Boolean) : Table = 
-	def projRows(row: List[Int], colPred: Int => Boolean, newRows: List[A], index: Int): List[Any] =
-		row match
-			case Nil => newRows
-			case h::t => if colPred(index) then projRows(t, colPred, newRows :: h, index + 1)
-				else projRows(t, colPred, newRows, index + 1)
-	table match 
-		case Nil => Nil
-		case h::t =>
-			projRows(h, colPred, List[], 0)
 
-	
+type Table = List[Tuple]
+
+def project(table: Table, colPred: (Int) => Boolean) : Table = {
+    def projRows(row: Tuple, colPred: Int => Boolean, newRows: Tuple, index: Int): Tuple = {
+        row match {
+            case EmptyTuple => newRows
+            case h*:t => 
+                if colPred(index) then projRows(t, colPred, newRows ++ Tuple(h), index + 1)
+                else projRows(t, colPred, newRows, index + 1)
+        }
+    }
+
+    table match { 
+        case Nil => Nil
+        case h::t =>
+            projRows(h, colPred, EmptyTuple, 0) :: project(t, colPred)
+        }
+}
+
+def projectCols(list:List[Int]): (Int) => Boolean = {
+    list match {
+        case Nil => (x:Int) => false
+        case h::t => (x:Int) => if x == h then true else projectCols(t)(x)
+    }
+}
 
 
-//def select(table: Table, rowPred: (Tuple) => Boolean) : Table = ???
+
+def select(table: Table, rowPred: (Tuple) => Boolean) : Table = {
+    def newTable(table: Table, rows: Table): Table = {
+        table match {
+            case Nil => rows.reverse
+            case h::t => 
+                if rowPred(h) then newTable(t, h :: rows)
+                else newTable(t, rows)
+        }
+    }
+    newTable(table, Nil)
+}
+
+def selectRows[A](idx:Int, pred: (A) => Boolean): (Tuple) => Boolean = {
+    def checkRows[A](idx: Int, pred:(A) => Boolean, row: Tuple): Boolean = {
+        def checkElements[A](idx: Int, row: Tuple): Option[A] = {
+            row match {
+                case EmptyTuple => None
+                case h*:t => 
+                    if idx == 0 then Some(h.asInstanceOf[A])
+                    else checkElements(idx - 1, t)
+            }
+        }
+        checkElements(idx, row) match {
+            case None => false
+            case Some(a) => pred(a)
+        }
+    }
+    (row: Tuple) => checkRows(idx, pred, row)
+}
+
+
+
+
 //def join(lhs: Table, rhs: Table, theta: (Tuple, Tuple) => Boolean) : Table = ???
 
 
