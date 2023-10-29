53ae812a15d016d6e89be681c888a8c2aacde57b
Kyle Lofthus <98364709+FidelisPyro@users.noreply.github.com>
2023-10-29 14:25:15 -0600

    Update DbOpsSuite.scala

--- DbOpsSuite.scala
+++ DbOpsSuite.scala
@@ -96,32 +96,261 @@ class DbOpsSuite extends ScalaCheckSuite {
         println("\n" + obtained + "\n")
     }
 
-    test("Testing select with something in index 0") {
-        //val rowPred: Tuple => Boolean = selectRows(2, (age: Int) => age > 25)
+    test("Testing select with an equality predicate") {
         
-        val obtained = select(emps, selectRows(2, (age: Int) => age == 25))
+        val obtained = select(emps, selectRows(2, (Age: Int) => Age == 25))
 
         val expected: Table = 
-            List((3,"Wilma", 25, 95000.0),
-                 (4,"Fred", 25, 90000.0))
+            List((4,"Fred", 25, 90000.0),
+                 (3,"Wilma", 25, 95000.0))
 
         assertEquals(obtained, expected)
 
         println("\n" + obtained + "\n")
     }
 
-    test("Testing select with something in index 0") {
-        //val rowPred: Tuple => Boolean = selectRows(2, (age: Int) => age > 25)
+    test("Testing select with a string match predicate") {
         
-        val obtained = select(emps, selectRows(1, (name: String) => name.substring(0, 1) == "B"))
+        val obtained = select(depts, selectRows(2, (Name: String) => Name == "Laramie"))
 
         val expected: Table = 
-            List((1,"Barney", 30, 100000.0),
-                 (2,"Betty", 20, 125000.0))
+            List((2,"Transportation", "Laramie"))
 
         assertEquals(obtained, expected)
 
         println("\n" + obtained + "\n")
+    }
+
+    test("Testing select with a partial string match predicate") {
+        
+        val obtained = select(emps, selectRows(1, (Name: String) => Name.startsWith("B")))
+
+
+        val expected: Table = 
+            List((2,"Betty", 20, 125000.0),
+                 (1,"Barney", 30, 100000.0))
+
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Testing join(theta)") {
+        val obtained = join(emps, worksIn, (emp: Tuple, work: Tuple) => emp.productElement(0) == work.productElement(0))
+
+        val expected: Table =
+            List((1,"Barney",30,100000.0,1,1),
+                 (1,"Barney",30,100000.0,1,2), 
+                 (2,"Betty",20,125000.0,2,2), 
+                 (2,"Betty",20,125000.0,2,3), 
+                 (3,"Wilma",25,95000.0,3,3), 
+                 (3,"Wilma",25,95000.0,3,4), 
+                 (4,"Fred",25,90000.0,4,4), 
+                 (4,"Fred",25,90000.0,4,1))
+
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Testing equiJoin") {
+        val obtained = join(emps, worksIn, equiJoin(0, 0)) 
+
+        val expected: Table =
+            List((1,"Barney",30,100000.0,1,1),
+                 (1,"Barney",30,100000.0,1,2), 
+                 (2,"Betty",20,125000.0,2,2), 
+                 (2,"Betty",20,125000.0,2,3), 
+                 (3,"Wilma",25,95000.0,3,3), 
+                 (3,"Wilma",25,95000.0,3,4), 
+                 (4,"Fred",25,90000.0,4,4), 
+                 (4,"Fred",25,90000.0,4,1))
+
+        assertEquals(obtained, expected)                 
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Testing andConds") {
+        val preds = List(
+            (t: Tuple) => t.productElement(0).asInstanceOf[Int] > 0,
+            (t: Tuple) => t.productElement(1).asInstanceOf[Any].toString.startsWith("B")
+        )
+
+        val obtained = select(emps, andConds(preds))
+        val expected: Table = 
+            List((2,"Betty",20,125000.0),                 
+                 (1,"Barney",30,100000.0))
+
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Testing andConds2") {
+        val preds = List(
+            (t: Tuple) => t.productElement(2).asInstanceOf[Int] < 30,
+            (t: Tuple) => t.productElement(3).asInstanceOf[Double] > 90000.0
+        )
+
+        val obtained = select(emps, andConds(preds))
+        val expected: Table =
+            List((3, "Wilma", 25, 95000.0),
+                 (2, "Betty", 20, 125000.0))
+
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Testing andConds3") {
+        val preds = List(
+            (t: Tuple) => t.productElement(0).asInstanceOf[Int] > 1,
+            (t: Tuple) => t.productElement(1).asInstanceOf[Int] < 4
+        )
+
+        val obtained = select(worksIn, andConds(preds))
+        val expected: Table =
+            List((4, 1),
+                 (3, 3),
+                 (2, 3),
+                 (2, 2))
+
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Testing orConds") {
+        val preds = List(
+            (t: Tuple) => t.productElement(1).asInstanceOf[Any].toString.startsWith("B"),
+            (t: Tuple) => t.productElement(3).asInstanceOf[Double] > 90000.0
+        )
+
+        val obtained = select(emps, orConds(preds))
+        val expected: Table =
+            List((3, "Wilma", 25, 95000.0),
+                 (2, "Betty", 20, 125000.0),
+                 (1, "Barney", 30, 100000.0))
+        
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Testing orConds2") {
+        val preds = List(
+            (t: Tuple) => t.productElement(2).asInstanceOf[String] == "Laramie",
+            (t: Tuple) => t.productElement(2).asInstanceOf[String] == "Denver"
+        )
+
+        val obtained = select(depts, orConds(preds))
+        val expected: Table =
+            List((3, "Sales", "Denver"),
+                 (2, "Transportation", "Laramie" ))
+        
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Name and age of those who make >= 100000.0") {
+        val colPred: Int => Boolean = (index: Int) => index == 1 || index == 2
+
+        val filteredEmps = select(emps, selectRows(3, (Salary: Double) => Salary >= 100000.0))
+        val obtained = project(filteredEmps, colPred)
+
+        val expected: Table =
+            List(("Betty", 20),
+                 ("Barney", 30))
+    
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Name and salary of those who make > 25") {
+        val colPred: Int => Boolean = (index: Int) => index == 1 || index == 3
+
+        val filteredEmps = select(emps, selectRows(2, (Age: Int) => Age > 25))
+        val obtained = project(filteredEmps, colPred)
+
+        val expected: Table =
+            List(("Barney", 100000.0))
+    
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Employee Name and their Department") {
+        val joined = join(emps, worksIn, equiJoin(0, 0))
+
+        val obtained = project(joined, projectCols(List(1, 5)))
+
+        val expected: Table =
+            List(("Barney", 1),
+                 ("Barney", 2),
+                 ("Betty", 2),
+                 ("Betty", 3),
+                 ("Wilma", 3),
+                 ("Wilma", 4),
+                 ("Fred", 4),
+                 ("Fred", 1))
+
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Employee List and Department List") {
+        val joined = join(emps, depts, equiJoin(0, 0))
+
+        val obtained = project(joined, projectCols(List(1, 5)))
+
+        val expected: Table =
+            List(("Barney", "Quarry"),
+                 ("Betty", "Transportation"),
+                 ("Wilma", "Sales"),
+                 ("Fred", "Abacus"))
+
+        assertEquals(obtained, expected)
+
+        println("\n" + obtained + "\n")
+    }
+
+    test("Employee Name their Department") {
+        val joined = join(emps, worksIn, equiJoin(0, 0))
+        val joined_trim = project(joined, projectCols(List(1, 5)))
+        val joined2 = join(joined_trim, depts, equiJoin(1, 0))
+
+        val joined2_test = join(joined, depts, (emp_dep: Tuple, deps: Tuple) => emp_dep.productElement(1) == deps.productElement(0))
+
+        //val obtained = project(joined2, projectCols(List(1, 5)))
+
+        val expected: Table =
+            List(("Barney", "Quarry"),
+                 ("Barney", "Transportation"),
+                 ("Betty", "Transportation"),
+                 ("Betty", "Sales"),
+                 ("Wilma", "Sales"),
+                 ("Wilma", "Abacus"),
+                 ("Fred", "Abacus"),
+                 ("Fred", "Quarry"))
+
+        println("\nJoined: " + joined + "\n")
+        println("\nJoined_trim: " + joined_trim + "\n")
+        println("\nJoined2: " + joined2 + "\n")
+        println("\nJoined2_test: " + joined2_test + "\n")
+        //println("\nObtained: " + obtained + "\n")
+
+        //assertEquals(obtained, expected)
+
+        //println("\n" + joined + "\n")
+        //println("\n" + joined2 + "\n")
+        //println("\n" + obtained + "\n")
     }
 }
+
+
 
